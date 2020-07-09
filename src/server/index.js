54cb3509b043610eb195df5e481d460a3856ac4c
Sebas <sebas.warsaw@gmail.com>
2020-07-09 13:41:23 -0300

    blah

--- src/server/index.js
+++ src/server/index.js
@@ -8,8 +8,11 @@ import Cookies from "cookies";
 
 const publicPath = path.join(__dirname, "/public");
 const app = express.Router({ mergeParams: true });
+const options = { extensions: false };
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath, options));
+app.use("/static/css", express.static(publicPath, options));
+app.use("/static/js", express.static(publicPath, options));
 app.use(Cookies.express());
 app.use(htmlMiddleware());
 app.use(storeMiddleware());
