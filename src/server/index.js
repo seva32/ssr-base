e74bac7783bf7274b3ab73a7a3e98cea185f33d1
Sebas <sebas.warsaw@gmail.com>
2020-07-09 11:52:30 -0300

    lost

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,16 @@
 import path from "path";
 import express from "express";
+import Cookies from "cookies";
 
 import htmlMiddleware from "./middleware/html";
 import storeMiddleware from "./middleware/store";
 import renderMiddleware from "./middleware/render";
-import Cookies from "cookies";
+import logger from "./middleware/logger";
 
 const publicPath = path.join(__dirname, "/public");
 const app = express.Router({ mergeParams: true });
 
+app.use(logger);
 app.use(express.static(publicPath));
 app.use(Cookies.express());
 app.use(htmlMiddleware());
